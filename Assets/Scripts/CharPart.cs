67606a618c097db314c5d43e7c3fde8006c1c81e
ahmet <abayir41@gmail.com>
2022-05-09 09:10:04 +0300

    Char Parts for new char

---
+++ Assets/Scripts/CharPart.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharPart : MonoBehaviour
+{
+    public ScaleMode Mode => mode;
+    [SerializeField] private ScaleMode mode;
+
+    
+}
